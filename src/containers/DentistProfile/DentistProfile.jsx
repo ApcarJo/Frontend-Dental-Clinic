6ee9456fa831e26b33acae263dd142af57b42176
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-10 18:12:24 +0200

    dentist profile created with css and jsx documents

--- src/containers/DentistProfile/DentistProfile.jsx
+++ src/containers/DentistProfile/DentistProfile.jsx
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import DentistNavbar from '../../components/DentistNavbar/DentistNavbar';
+import imgUser from '../../img/user.png';
+import './DentistProfile.css'
 
 
 const DentistProfile = () => {
 
-    return(
-        <div>
-            SOY DENTIST PROFILE
-        </div>
-    )
+  
+    let history = useHistory() ;
+
+    //hooks
+    const [dentistData, setDentistData] = useState({
+        token: localStorage.getItem("token"), 
+        dentist: JSON.parse(localStorage.getItem("dentist"))
+    });
+
+    useEffect( () => {
+
+    }, [])
+
+    useEffect( () => {
+
+    })
+
+    const logout = () => {
+        localStorage.clear();
+        setDentistData("");
+    }
+
+    if(dentistData.token) {
+
+        return(
+            <div className="dentistProfile">
+                <DentistNavbar />
+                <div className="dentistDates">
+                    <div className="dentistLeftSide">
+                        <h2>MY PROFILE</h2>
+                        <img src={imgUser} alt="user" className="imgUser"/>
+                        <div className="buttons">
+                            <div className="buttonUpdate">UPDATE</div>
+                            <div className="buttonLogout" onClick={() => logout()}>LOGOUT</div>
+                        </div>
+                    </div>
+                    <div className="dentistRightSide">
+                        <p>NAME : {dentistData.dentist.name} </p>
+                        <p>EMAIL : {dentistData.dentist.email} </p>
+                        <p>PHONE : {dentistData.dentist.phone}</p>
+                        <p>SPECIALITY : {dentistData.dentist.speciality}</p>
+                        <p>CITY : {dentistData.dentist.city}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
+
+        setTimeout(() => {
+    
+            history.push("/login")
+
+       }, 2000);
+ 
+        return(
+            <div>
+                CARGANDO DATOS
+            </div>
+        )
+
+    }
+
+
 
 }
 
