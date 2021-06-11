33caa71dc0690359bf62dd9dfd7ac8a4ca59c09c
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-11 19:09:29 +0200

    Change state hook

--- src/containers/Appointment/CreateAppointment.jsx
+++ src/containers/Appointment/CreateAppointment.jsx
@@ -1,19 +1,35 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-
 import axios from "axios";
-
 import "./CreateAppointment.css";
 
+
 const CreateAppointmnet = () => {
+
   let history = useHistory();
 
   //Hooks
   const [datos, setDatos] = useState({
-    client: "datos.client",
-    clinic: "datos.clinic",
-    dentist: "datos.dentist",
-    date: "datos.date",
+    client: "",
+    clinic: "",
+    dentist: "",
+    date: "",
+    message: ""
+  });
+
+//   const [userData, setUserData] = useState({
+
+//     client: JSON.parse(localStorage.getItem("client")),
+//     token: localStorage.getItem("token")
+// });
+
+  useEffect( () => {
+
+  }, []);
+
+  useEffect( () => {
+
+    crearCita();
   });
 
   //Handlers
@@ -22,36 +38,24 @@ const CreateAppointmnet = () => {
   };
 
   const crearCita = async () => {
-    // {
-    //     "client": "60bc9fdc76beb507fd3fcd78",
-    //     "clinic": "60bb5e51ce1da1086f556d20",
-    //     "dentist": "60b8bbd4006369390083b4ce",
-    //     "date": "2022-06-06",
-    //     "isActive": true
-    // }
+
+    let token = localStorage.getItem("token")
 
     let body = {
       client: datos.client,
       clinic: datos.clinic,
       dentist: datos.dentist,
       date: datos.date,
+      message: datos.message
     };
 
-    let res = await axios.post("http://localhost:3006/appointment", body);
+    let res = await axios.post("http://localhost:3006/appointment", body, {headers:{'authorization':'Bearer ' + token}});
 
-    let client = res.data.client;
-    let token = res.data.token;
+    console.log(res.data.results);
+    setTimeout(()=>{
+      history.push("/clientprofile");
+  },750);
 
-    if (token !== "") {
-      localStorage.setItem("token", token);
-      localStorage.setItem("client", JSON.stringify(client));
-
-      setTimeout(() => {
-        history.push("/profile");
-      }, 750);
-    } else {
-      console.log("Las credenciales no eran correctas");
-    }
   };
 
   return (
