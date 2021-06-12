0fdb3976b4581f3283d4e982e4bc179d1de9c21b
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-12 18:07:22 +0200

    register menu created where you can choose client or dentist

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import './Register.css';
+import {useHistory} from 'react-router-dom';
 
 
 const Register = () => {
 
+    let history = useHistory();
+
+    const goTo = (path) => {
+
+        history.push(path);
+
+    }
+
     return(
-        <div>
-            SOY REGISTER
+        <div className="registerHome">
+            
+            <div className="registerOptions">
+
+                <h1>WELCOME</h1>   
+                <h3>Are you a new dentist?</h3>
+                <div className="buttonRegister" onClick={() => goTo("/dentistregister")}>CLICK</div> 
+                <h3>Are you a new client?</h3>
+                <div className="buttonRegister" onClick={() => goTo("/clientregister")}>CLICK</div> 
+
+            </div>
         </div>
     )
 
