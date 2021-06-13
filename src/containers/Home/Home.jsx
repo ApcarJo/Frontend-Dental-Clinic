2b771e5e11fc931a3510b9c67c80230ccd447222
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-13 18:07:57 +0200

    Add img to test on home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+// import home from '../../assets/clinic/home.jpeg';
+import './Home.css';
+
 
 
 const Home = () => {
 
     return(
-        <div>
-            SOY HOME
+        <div className="clinicImg">
+            <div>
+                <img  className="clinic" alt="clinic"/>
+            </div>
         </div>
     )
 
