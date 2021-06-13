c307c1db11647dc732973530e9554af6ebdb0a0a
José Luis <hansokaz@gmail.com>
2021-06-13 17:25:42 +0200

    added login roles

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -11,7 +11,8 @@ const Login = () => {
 
     // Hooks
     const [credentials,setCredentials] = useState({email:'',password:''});
-    const [msgError, setMensajeError] = useState('');
+    const [msgError, setMensajeError] = useState({eEmail:'',ePassword: '',eValidate:''});
+    const [statusRole, setStatusRole] = useState({roleStatus: ''});
 
     // Esto es un Handler
     const updateCredentials = (e) => {
@@ -32,6 +33,41 @@ const Login = () => {
         
     });
 
+    const checkStatus = async () => {
+
+        let body = {
+            email: credentials.email
+        }
+
+        let role = await axios.post('http://localhost:3006/clients/email', body);
+
+        if (role.data !== null){
+            setStatusRole({...statusRole, roleStatus: 'client'});
+        }
+
+        if (role.data == null){
+            role = await axios.post('http://localhost:3006/dentists/email', body);
+            if (role.data !== null) { 
+                setStatusRole({...statusRole, roleStatus: 'dentist'});
+            } 
+        }
+    }
+
+    const checkError = () => {
+
+        if (credentials.email.lengt < 3){
+            setMensajeError({...msgError, eEmail: "Introduce un email"});
+        }else {
+            setMensajeError({...msgError, eEmail: ""});
+        }
+
+        if (credentials.password.length < 1){
+            setMensajeError({...msgError, ePassword: "Introduce un password"});
+        }else {
+            setMensajeError({...msgError, ePassword: ""});
+        }
+    }
+
     const logeame = async () => {
 
         // Primero, testeamos los datos
@@ -40,42 +76,32 @@ const Login = () => {
         //     this.muestraError("Introduce un e-mail válido.");
         //     return;
 
-        if (credentials.email.lengt < 4){
-            setMensajeError("Introduce un email válido");
-            return;
-        }
-
-        if (credentials.password.length < 4){
-            setMensajeError("El password no es válido")
-            return;
-        }
         // A continuamos, generamos el body de datos
 
         let body = {
             email : credentials.email,
             password : credentials.password
         }
-
-        console.log("enviaría este body", body);
-
         // Envío por axios
-
-        let res = await axios.post('http://localhost:3006/login/Client', body);
+        console.log(body)
+        let res = await axios.post(`http://localhost:3006/login/${statusRole.roleStatus}`, body);
+        console.log(res, "este es ressssssssssssssssssssssss");
         // let id = res.data.client._id;
         let token = res.data.token;
-
+        console.log(token, "este es el tokeeeeeeeeeeeeeeeeeeeeeeeeeeeeen");
 
         if (token !== ""){
             // A falta de redux vamos a usar LocalStorage
 
             localStorage.setItem("token", token);
             localStorage.setItem("client", JSON.stringify(res.data.client));
+            localStorage.setItem("dentist", JSON.stringify(res.data.dentist));
         // redirección
             setTimeout(()=>{
-                history.push("/clientprofile");
+                history.push(`/${statusRole.roleStatus}profile`);
             },750);
         }else {
-            setMensajeError("Las credenciales no eran correctas");
+            setMensajeError({...msgError, eValidate: 'Wrong email or password'});
         }
         // res viene de vuelta con el token y los datos
     }
@@ -87,10 +113,12 @@ const Login = () => {
         <div className="vistaLogin">
                 <pre>{JSON.stringify(credentials, null,2)}</pre>
                 <div className="loginCard">
-                    <input type='email' className='loginBox' name='email' onChange={updateCredentials}  placeholder="your@email.net"></input>
-                    <input type='text' className='loginBox' name='password' onChange={updateCredentials}    placeholder="password"></input>
+                    <input type='text' className='loginBox' name='email' onChange={updateCredentials} onBlur={()=>checkStatus("email")} placeholder="your@email"></input>
+                    <div>{msgError.eEmail}</div>
+                    <input type='text' className='loginBox' name='password' onChange={updateCredentials} onBlur={()=>checkError("name")}  placeholder="password"></input>
+                    <div>{msgError.ePassword}</div>
                     <div className="sendButton" onClick={()=>logeame()}>Login</div>
-                    <div>{msgError}</div>
+                    <div>{msgError.eValidate}</div>
                     {/* <div className="receiveInfo" onClick={()=>receive()}>ReceiveInfo</div> */}
                 </div>
         </div>
