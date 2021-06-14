069f91460f6fe4cc5912f5f92e6ce5cd534f0b2d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-14 17:02:29 +0200

    Change files client appointmen

--- src/containers/ClientAppointments/ClientAppointments.jsx
+++ src/containers/ClientAppointments/ClientAppointments.jsx
@@ -3,7 +3,6 @@ import ClientNavbar from '../../components/ClientNavbar/ClientNavbar';
 import './ClientAppointments.css';
 import axios from 'axios';
 const ClientAppointments = () => {
-<<<<<<< HEAD
     //hooks
     const [clientAppointment, setClientAppointment] = useState({});
     useEffect( () => {
@@ -51,53 +50,5 @@ const ClientAppointments = () => {
             </div>
         )
     }
-=======
-    //hooks
-    const [clientAppointment, setClientAppointment] = useState({});
-    useEffect( () => {
-        searchAppointments();
-    }, [])
-    useEffect( () => {
-    })
-    const searchAppointments = async () => {
-        try{
-            let token = localStorage.getItem('token');
-            let user = JSON.parse(localStorage.getItem('client'));
-            let body = {
-                client: user._id,
-            }
-            let res = await axios.post('http://localhost:3006/appointment/client',body, {headers:{'authorization':'Bearer ' + token}});
-            setClientAppointment(res.data)
-        } catch (error){
-            console.log(error)
-        }
-    }
-    if(clientAppointment[0]?.clinicName){
-        // si existe, mapeamos los resultados
-        return(
-            <div className='clientAllAppointmets'>
-                <ClientNavbar />
-                <h1>MY APPOINTMENTS</h1>
-                <div className='appointmentsContainer'>
-                    {clientAppointment.map((appointment, index) => (
-                        <div key={index} className='appointmentCard'>
-                            <p> CLINIC : {appointment.clinicName} </p>
-                            <p> PHONE : {appointment.clinicPhone} </p>
-                            <p> EMAIL : {appointment.clinicEmail} </p>
-                            <p> DENTIST : {appointment.dentistName} </p>
-                            <p> DATE : {appointment.date} </p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
-    }else {
-        return(
-            <div>
-                ESTOY CARGANDO
-            </div>
-        )
-    }
->>>>>>> 99f37b21ad2c4d8837c9d43ebf4d4cd4f9837d90
 }
 export default ClientAppointments;
