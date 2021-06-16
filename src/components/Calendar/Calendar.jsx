3cb8d00d5793597d69df90493f06b8ce5ac87199
José Luis <hansokaz@gmail.com>
2021-06-16 18:23:08 +0200

    drawing the week

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -37,11 +37,43 @@ const Calendar = () => {
 
 	});
 
-    const anyos = (anyo, dia, mes1) => {
-		let x=0, b=0;
-		let principal=1;
-		let dias;
-		let semana = [];
+	
+
+	const men20 = (a) => {
+		a=a-1;
+		let numwrite = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "dieciocho", "diecinueve", "veinte"]
+		arrayDate += numwrite[a];
+	}
+
+	const dec = (decenas) => {
+		if(decenas===2)
+		arrayDate += "veinti";
+		
+
+		if(decenas===3)
+		arrayDate += "treinta y ";
+		
+	}
+
+	const dec2 = (decenas) => {
+			if(decenas===3)
+			arrayDate += "treinta";
+	}
+
+	const unid = (unidades) => {
+		let arrayNum = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", 	"ocho", "nueve"]
+		unidades=unidades-1;
+		arrayDate += (arrayNum[unidades]);
+	}
+
+	let dia=parseInt(datosUser.dia);
+    let mes=parseInt(datosUser.mes);
+    let anyo=parseInt(datosUser.anyo);
+
+	let x=0, b=0;
+	let principal=1;
+	let dias;
+	let semana = [];
 
 		for (principal=1; principal<anyo; principal++){
 			x++;
@@ -49,47 +81,6 @@ const Calendar = () => {
 				b++;
 			}
 		}
-		dias=(b*366)+((x-b)*365)+(dia)+(mes1);
-		let i=dias%7;
-		semana = ["Domingo, ", "Lunes, ", "Martes, ", "Miércoles, ", "Jueves, ", "Viernes", "Sábado"];
-		arrayDate = semana[i];
-		}
-		const men20 = (a) => {
-			a=a-1;
-			let numwrite = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once", "doce", "trece", "catorce", "quince", "dieciseis", "diecisiete", "dieciocho", "diecinueve", "veinte"]
-			arrayDate += numwrite[a];
-			
-
-	}
-	
-	const dec = (decenas) => {
-			if(decenas===2)
-			arrayDate += "veinti";
-			
-	
-			if(decenas===3)
-			arrayDate += "treinta y ";
-			
-	}
-	
-	const dec2 = (decenas) => {
-			if(decenas===3)
-			arrayDate += "treinta";
-	}
-	
-	const unid = (unidades) => {
-	
-		let arrayNum = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"]
-		unidades=unidades-1;
-		
-		arrayDate += (arrayNum[unidades]);
-	}
-
-	let mese = (mes, dia, mes1, anyo) => {
-		let arrayMes = [" de Enero", " de Febrero", " de Marzo", " de Abril", " de Mayo", " de Junio", " de Julio", " de Agosto", " de Septiembre", " de Octubre", " de Noviembre", " de Diciembre"];
-		arrayDate += (arrayMes[mes-1]);
-		
-
 		let mesi = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         if ((anyo%4===0 && anyo%100!==0) || (anyo%400===0)) {
 			mesi[1]=29;
@@ -97,22 +88,27 @@ const Calendar = () => {
 			mesi[1]=28;
 		}
 
-		let i;
+		
 
-		mes=mes-1;
-
-		let x=0;
-		let b=0;
-		let resto1=0;
-		let dias=0;
-		let principal=1;
-
-		for (principal=1; principal<anyo; principal++) {
-			x++;
-			if ((principal%4===0 && principal%100!==0) || (principal%400===0)) {
-				b++;
+		let days=0;
+	
+		for (let i=0; i<mes-1; i++)
+			{
+				days=days+mesi[i];
 			}
-		}
+		let mes1=days;
+		dias=(b*366)+((x-b)*365)+(dia)+(mes1);
+		let i=dias%7-1;
+		semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+		arrayDate = semana[i];
+
+		let arrayMes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+		arrayDate += (arrayMes[mes-1]);
+
+		let resto1=0;
+		dias=0;
+		
+
 		dias=(b*366)+((x-b)*365)+(dia)+(mes1);
 
 		resto1=(dias-dia)%7;
@@ -121,133 +117,67 @@ const Calendar = () => {
 			resto1=resto1*-1;
 		}
 
-		let a = [1, 2, 3, 4, 5, 6, 7];
-		
-		if (resto1===0)
-		{
-		a=1;
-		}
-		if (resto1===1)
-		{
-		console.log("\t");
-		a=2;
-		}
-		if (resto1===2)
-		{
-		console.log("\t\t");
-		a=3;
-		}
-		if (resto1===3)
-		{
-		console.log("\t\t\t");
-		a=4;
-		}
-		if (resto1===4)
-		{
-		console.log("\t\t\t\t");
-		a=5;
-		}
-		if (resto1===5)
-		{
-		console.log("\t\t\t\t\t");
-		a=6;
-		}
-		if (resto1===6)
-		{
-		console.log("\t\t\t\t\t\t");
-		a=7;
-		}            
+
+		let a = [' ', "\t", "\t\t", "\t\t\t", "\t\t\t\t", "\t\t\t\t\t", "\t\t\t\t\t\t"];
 
 
-	for (i=1; i<=mesi[mes]; i++)
+	for (i=1; i<=mesi[mes-1]; i++)
 	{
-		if (dia===i)
-			{
-			console.log("%d*\t", i);
-			}
-
-		if (dia!==i)
-			{
-			console.log("%d\t", i);
-			}
-
-		//Inicia la siguiente linea de dias a partir del domingo.
-
-		if (a%7===0)
-			{
-			console.log(" ");
-			}
-			a++;
-	}
-}
-
-const meses = (mes, anyo) => {
-
-	let mesi = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if ((anyo%4===0 && anyo%100!==0) || (anyo%400===0))
-		mesi[1]=29;
-		else
-		mesi[1]=28;
-
-	let i;
-	let days=0;
-
-	for (i=0; i<mes-1; i++)
-		{
-			days=days+mesi[i];
+		if (dia===i){
+			console.log(i+'*');
 		}
 
-	// setDatosUser({...datosUser, days: days});
-	return days;
+		if (dia!==i){
+			console.log(i);
+		}
 
-}
+		if (a%7===0){
+			console.log(" ");
+		}
+		a++;
+	}
+
+	let diasMes=[];
+	for (i=1; i<=mesi[mes]; i++){
+		diasMes.push(i);
+	};
+	
 
 
-
-	let dia, mes, anyo, mes1,c;
-	let resto1;
-
-	dia=parseInt(datosUser.dia);
-    mes=parseInt(datosUser.mes);
-    anyo=parseInt(datosUser.anyo);
-
-	mes1=meses(mes, anyo);
-	anyos(anyo, dia, mes1);
-
+	i=0;
+	
+	
 	let unidades;
 	let decenas;
 
-
 	if (dia<=20)
-		{
-			men20(dia);
-		}
+		men20(dia);
 
 	decenas=parseInt(dia/10);
 	unidades=dia%10;
 
-	if (dia>20)
+	if (dia>20){
+		if (unidades===0)
+			dec2(decenas);
 
-		{
-			if (unidades===0)
-					dec2(decenas);
+		if (unidades!==0){
+			decenas=parseInt(dia/10);
+			dec(decenas);
 
-			if (unidades!==0)
-					{
-
-					decenas=parseInt(dia/10);
-					dec(decenas);
-
-					unidades=dia%10;
-					unid(unidades);
-				}
+			unidades=dia%10;
+			unid(unidades);
 		}
+	}
 
-	mese(mes, dia, mes1, anyo);
+	let calendarObject = {
+		dia: dia,
+		semana: semana,
+		dias: diasMes,
+		mes: mes,
 
-	console.log(arrayDate, 'de', anyo)
-	
-	// scanf("%d", &c);
+	}
+	console.log(calendarObject)
+	console.log(arrayDate)
 
 
 return (
@@ -259,44 +189,34 @@ return (
 
 
 		<div className="drawCalendar">
-			<div className="dayBox">Lunes</div>
-			<div className="dayBox">Martes</div>
-			<div className="dayBox">Miércoles</div>
-			<div className="dayBox">Jueves</div>
-			<div className="dayBox">Viernes</div>
-			<div className="dayBox">Sábado</div>
-			<div className="dayBox">Domingo</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
-			<div className="dayBox">Hola</div>
+
+		{semana.map((semana, index) => (
+
+
+				<div className="" key={index}>
+					
+					<div className="dayBox">
+						<p>{semana} </p>
+						
+					</div>
+				</div>
+
+		))}
+
+		{diasMes.map((diasMes, index) => (
+
+
+			<div className="dayBox" key={index}>
+
+				<div className="texto">
+					<p>{diasMes} </p>
+
+				</div>
+
+		</div>
+		))}
+
+			
 			
 		</div>
 	</div>
