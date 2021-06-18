bc9952aa086214e4160dc64574d605c32ab62ed3
ApcarJo <hansokaz@gmail.com>
2021-06-18 13:27:27 +0200

    Accept merge in typesç

--- src/redux/types.js
+++ src/redux/types.js
@@ -2,12 +2,7 @@
 export const LOGIN = 'login'
 export const LOGOUT = 'logout'
 
-<<<<<<< HEAD
-export const LOGOUT = 'logout'
-
 export const UPDATE_USER = 'update_user'
-=======
 export const DRAW_CAL = 'draw_cal'
 export const DATE_CAL = 'date_cal'
 export const REMOVE_CAL = 'remove_cal'
->>>>>>> feature/SCHEDULEDENTIST
