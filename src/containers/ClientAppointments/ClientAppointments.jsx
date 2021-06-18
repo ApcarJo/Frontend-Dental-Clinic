0ad490e4dd07004f245625093c21e3567a7f590a
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-18 18:59:22 +0200

    Create file updateAppointment with result 200

--- src/containers/ClientAppointments/ClientAppointments.jsx
+++ src/containers/ClientAppointments/ClientAppointments.jsx
@@ -1,64 +1,92 @@
-import React, {useState, useEffect} from 'react';
-import './ClientAppointments.css';
-import axios from 'axios';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import "./ClientAppointments.css";
+import axios from "axios";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { APPOINTMENT } from "../../redux/types";
 
 const ClientAppointments = (props) => {
-    //hooks
-    const [clientAppointment, setClientAppointment] = useState({});
-    useEffect( () => {
-        searchAppointments();
-    }, [])
-     useEffect( () => {
-       
-        }, [])
-    const searchAppointments = async () => {
-        try{
-            let token = props.credentials?.token;
-            let user = props.credentials?.client;
+  let history = useHistory(); //hooks
 
-            let body = {
-                client: user._id,
-            }
+  const [clientAppointment, setClientAppointment] = useState({});
 
-            let res = await axios.post('http://localhost:3006/appointment/client',body, {headers:{'authorization':'Bearer ' + token}});
-            console.log('here', res.data);
-            setClientAppointment(res.data)
+  useEffect(() => {
+    searchAppointments();
+  }, []);
 
-        } catch (error){
-            console.log(error)
+  useEffect(() => {}, []);
 
-        }
-    }
-    if(clientAppointment[0]?.clinicName){
-        // si existe, mapeamos los resultados
-        return(
-            <div className="clientAllAppointmets">
-                <h1>MY APPOINTMENTS</h1>
-                <div className="appointmentsContainer">
-                    {clientAppointment.map((appointment, index) => (
-                        <div key={index} className="appointmentCard">
-                            <p> CLINIC : {appointment.clinicName} </p>
-                            <p> PHONE : {appointment.clinicPhone} </p>
-                            <p> EMAIL : {appointment.clinicEmail} </p>
-                            <p> DENTIST : {appointment.dentistName} </p>
-                            <p> DATE : {appointment.date} </p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
-    }else {
-        return(
-            <div>
-                ESTOY CARGANDO
-            </div>
-        )
-    }
-}
+  const searchAppointments = async () => {
+    try {
+      let token = props.credentials?.token;
+      let user = props.credentials?.client;
+
+      let body = {
+        client: user._id,
+      };
+
+      let res = await axios.post(
+        "http://localhost:3006/appointment/client",
+        body,
+        { headers: { authorization: "Bearer " + token } }
+      );
+      // console.log('here', res.data);
+      setClientAppointment(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const saveAppointment = (appointment) => {
+    let newApp = appointment;
+
+    props.dispatch({ type: APPOINTMENT, payload: newApp });
+
+    history.push("/updateappointments");
+  };
+
+  if (clientAppointment[0]?.id) {
+    // si existe, mapeamos los resultados
+    return (
+      <div className="clientAllAppointmets">
+                        <h1>MY APPOINTMENTS</h1>
+                        
+        <div className="appointmentsContainer">
+                              
+          {clientAppointment.map((appointment, index) => (
+            <div key={index} className="appointmentCard">
+                                          
+              <p> CLINIC : {appointment.clinicName} </p>
+                                          
+              <p> PHONE : {appointment.clinicPhone} </p>
+                                          
+              <p> EMAIL : {appointment.clinicEmail} </p>
+                                          
+              <p> DENTIST : {appointment.dentistName} </p>
+                                          
+              <p> DATE : {appointment.date} </p>
+              <div className="buttons1">
+                <div
+                  className="buttonUpdateC1"
+                  onClick={() => saveAppointment(appointment)}
+                >
+                  UPDATE
+                </div>
+                <div className="buttonLogoutC1">REMOVE</div>
+              </div>
+                                     
+            </div>
+          ))}
+                          
+        </div>
+                    
+      </div>
+    );
+  } else {
+    return <div>                ESTOY CARGANDO             </div>;
+  }
+};
 
 export default connect((state) => ({
-
-    credentials:state.credentials
-
-    }))(ClientAppointments);
+  credentials: state.credentials,
+}))(ClientAppointments);
