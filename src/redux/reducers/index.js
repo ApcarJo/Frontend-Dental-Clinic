267282f8a4bf43230e7292b4af78f8388d4bd642
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-18 18:56:48 +0200

    Create reducer appointment (WORKTEAM)

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import credentials from './credentials-reducer'
+import appointment from './appointment-reducer';
 
 const rootReducer = combineReducers ({
 
     // aquí importaremos todos los reducers:
-    credentials
-    
+    credentials,
+    appointment
 });
 
 export default rootReducer;
--- src/redux/types.js
+++ src/redux/types.js
@@ -3,3 +3,8 @@ export const LOGIN = 'login'
 export const LOGOUT = 'logout'
 
 export const UPDATE_USER = 'update_user'
+
+export const APPOINTMENT = 'appointment'
+
+export const UPDATE_APPOINTMENT = 'uptade_appoint'
+
