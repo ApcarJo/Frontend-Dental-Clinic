26b0acde56751dc4a4ab9e60d766ad8b10f613d7
AdrianaFayos <adrifayosperis@gmail.com>
2021-06-19 19:51:46 +0200

    home style updated

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = (props) => {
 
                     <div className="contenedorTexto">
                             <h1>T R I D E N T</h1>
-                            <p className="parrHome">In our clinics you will find the latest equipment to deliver the highest quality treatments, as well as comfortable atmosphere in order to make your stay more pleasant. We have also chosen the best suppliers to guarantee long lasting and solid treatments, delivered by a team of experienced professionals.</p>
+                            <p className="parrHome">In our clinics you will find the latest equipment to deliver the highest quality treatments, as well as comfortable atmosphere in order to make your stay more pleasant.</p>
                             <div className="buttonHome" onClick={() => goTo("/appointments")}>MAKE AN APPOINTMENT</div> 
                     </div>
                     <img className="fondo" src={home} alt="home" />
@@ -32,15 +32,18 @@ const Home = (props) => {
 
                 <div className="optionsHome">
                     <div className="option">
-                        <p className="parrafos">CLINICS</p>
+                        <p>CLINICS</p>
+                        <p className="parrafos">We have more than 100 clinics with everything you need to make your stay as pleasant as possible and solve all your dental needs with the greatest comfort. </p>
                         <div className="buttonOption" onClick={() => goTo("/clinics")}></div>
                     </div>
                     <div className="option">
-                    <p className="parrafos" >CONTACT</p>
+                        <p >CONTACT</p>
+                        <p className="parrafos">If you have any questions, do not doubt to contact our customer service department. We will be glad to help you!</p>
                         <div className="buttonOption" onClick={() => goTo("/contact")}></div>
                     </div>
                     <div className="option">
-                    <p className="parrafos">ABOUT US</p>
+                        <p>ABOUT US</p>
+                        <p className="parrafos">25 years of experience learning every day to solve the problems of our patients and solving totally hopeless cases.</p>
                         <div className="buttonOption" onClick={() => goTo("/aboutus")}></div>
                     </div>
                 </div>
